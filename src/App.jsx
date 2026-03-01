3996c199c9a0f6b19140d75c91018d1d1681e95e
Anish <anishkumar5080@gmail.com>
2026-03-01 21:33:48 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,65 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// import TopBarNotification from "./pages/TopBarComponents/TopBarNotification";
-// import TopBarMail from "./pages/TopBarComponents/TopBarMail";
+import TopBarNotification from "./pages/TopBarComponents/TopBarNotification";
+import TopBarMail from "./pages/TopBarComponents/TopBarMail";
 
-// import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 
-// import StudentProfile from "./pages/StudentManagement/StudentProfile";
-// import StudentEnrolment from "./pages/StudentManagement/StudentEnrollment";
+import StudentProfile from "./pages/StudentManagement/StudentProfile";
+import StudentEnrolment from "./pages/StudentManagement/StudentEnrollment";
 
-// import Exam from "./pages/ExamManagement/Exam";
+import Exam from "./pages/ExamManagement/Exam";
 
-// import AllCourse from "./pages/CourseManagement/AllCourse";
+import AllCourse from "./pages/CourseManagement/AllCourse";
 
-// import QuestionBank from "./pages/QuestionPaperPart/QuestionBank";
+import QuestionBank from "./pages/QuestionPaperPart/QuestionBank";
 
-// import RewardAwards from "./pages/Reward&Award/RewardAwards";
+import RewardAwards from "./pages/Reward&Award/RewardAwards";
 
-// import Leaderboard from "./pages/LeadBoardPart/Leaderboard";
+import Leaderboard from "./pages/LeadBoardPart/Leaderboard";
 
-// import Analytics from "./pages/AnalyticsPart/Analytics";
+import Analytics from "./pages/AnalyticsPart/Analytics";
 
-// import Transactions from "./pages/TransactionPart/Transactions";
+import Transactions from "./pages/TransactionPart/Transactions";
 
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
 
-//         <Route path="/notifications" element={<TopBarNotification />} />
-//         <Route path="/mail" element={<TopBarMail />} />
+        <Route path="/notifications" element={<TopBarNotification />} />
+        <Route path="/mail" element={<TopBarMail />} />
 
-//         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
-//         <Route
-//           path="/student-management/profile-view"
-//           element={<StudentProfile />}
-//         />
-//         <Route
-//           path="/student-management/enrollment"
-//           element={<StudentEnrolment />}
-//         />
+        <Route
+          path="/student-management/profile-view"
+          element={<StudentProfile />}
+        />
+        <Route
+          path="/student-management/enrollment"
+          element={<StudentEnrolment />}
+        />
 
-//         <Route path="/exam-management" element={<Exam />} />
+        <Route path="/exam-management" element={<Exam />} />
 
-//         <Route path="/course-management/all-courses" element={<AllCourse />} />
+        <Route path="/course-management/all-courses" element={<AllCourse />} />
 
-//         <Route path="/question-papers" element={<QuestionBank />} />
+        <Route path="/question-papers" element={<QuestionBank />} />
 
-//         <Route path="/rewards-awards" element={<RewardAwards />} />
+        <Route path="/rewards-awards" element={<RewardAwards />} />
 
-//         <Route path="/leader-board" element={<Leaderboard />} />
+        <Route path="/leader-board" element={<Leaderboard />} />
 
-//         <Route path="/analytics" element={<Analytics />} />
+        <Route path="/analytics" element={<Analytics />} />
 
-//         <Route path="/transaction" element={<Transactions />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
+        <Route path="/transaction" element={<Transactions />} />
+      </Routes>
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
